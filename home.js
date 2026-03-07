869238b010fdcdf0eee410fca344593cbb39f5af
Omi <seyam.islam020@gmail.com>
2026-03-08 00:12:19 +0600

    add button and open button are now fully functional

--- home.js
+++ home.js
@@ -1,7 +1,8 @@
 
 const issueNumber = document.getElementById('number-of-issues');
 const openButton = document.getElementById('open-button');
-const allButton = document.getElementById('all-button')
+const allButton = document.getElementById('all-button');
+const closedButton = document.getElementById('closed-button');
 
 const allIssues = () => {
     const url = 'https://phi-lab-server.vercel.app/api/v1/lab/issues';
@@ -24,6 +25,11 @@ openButton.addEventListener('click', openIssue);
 
 const displayOpenCards = (openCards) => {
 
+
+    allButton.classList.remove('btn-primary');
+    closedButton.classList.remove('btn-primary');
+    openButton.classList.add('btn-primary')
+
     const onlyOpen = openCards.filter(card => card.status === 'open');
     issueNumber.innerText = onlyOpen.length;
     const cardsContainer = document.getElementById('cards-container');
@@ -42,6 +48,35 @@ const displayOpenCards = (openCards) => {
             priority = 'bg-gray-200 text-gray-600';
         }
 
+        let greenBorder = '';
+        if (openCard.status === 'open') {
+            greenBorder = 'border-t-4 border-green-500'
+        }
+
+        const labels = openCard.labels.map(label => {
+            let color = '';
+            let icon = '';
+            if (label.toLowerCase() === 'bug') {
+                color = 'text-red-600 border-red-400 bg-red-100';
+                icon = '<i class="fa-solid fa-bug"></i>'
+            }
+            else if (label.toLowerCase() === 'enhancement') {
+                color = 'text-green-700 border-green-400 bg-green-100';
+                icon = '<i class="fa-solid fa-wand-magic-sparkles"></i>';
+            }
+            else if (label.toLowerCase() === 'help wanted') {
+                color = 'text-yellow-700 border-yellow-400 bg-yellow-100';
+                icon = '<i class="fa-regular fa-life-ring"></i>';
+            }
+            else if (label.toLowerCase() === 'documentation') {
+                color = 'text-blue-700 border-blue-400 bg-blue-100'
+            } else {
+                color = 'text-gray-700 border-gray-500 bg-gray-200'
+            }
+
+            return `<button class ="rounded-xl px-3 py-1 border ${color}"> ${icon} ${label}</button>`
+        }).join(' ');
+
         divOpenCard.innerHTML = `
 
     
@@ -53,20 +88,23 @@ const displayOpenCards = (openCards) => {
                 <div>
                     <h2 class="text-[#1F2937] text-xl font-medium mb-2">${openCard.title}</h2>
                     <p class="text-[#64748B] mb-2"><small>${openCard.description}</small></p>
-                    <button class=" rounded-xl text-red-600 border-red-400 px-3 py-1 border bg-red-100 "><i class="fa-solid fa-bug"></i> BUG</button>
-                    <button class=" rounded-xl text-yellow-700 border border-yellow-400 py-1 px-3 bg-yellow-100"><i class="fa-regular fa-life-ring"></i> HELP WANTED</button>
+                    <div class="flex gap-5 mb-2">
+                        ${labels}
+                    </div>
                 </div>
                 <hr class="bg-gray-400 mt-5">
 
                 <div class="mt-3 flex justify-between">
                     <div>
-                    <p class="text-[#64748B]"><small> #1 by John_doe </small></p>
+                    <p class="text-[#64748B]"><small> ${openCard.id} by ${openCard.author} </small></p>
                     <p class="text-[#64748B]"><small> 1/15/2025 </small></p> 
+                    
                     </div>
 
                     <div>
                     <p class="text-[#64748B]"> <small> ${openCard.createdAt} </small> </p>
                     <p class="text-[#64748B]"> <small> ${openCard.updatedAt} </small> </p>
+                     
                     </div> 
 
                 </div>
@@ -75,22 +113,25 @@ const displayOpenCards = (openCards) => {
 
 
     `
-    cardsContainer.append(divOpenCard);
+        cardsContainer.append(divOpenCard);
 
     })
-
-
 }
+
+
 
 const displayCardsinAll = (cards) => {
     issueNumber.innerText = cards.length;
-      const cardsContainer = document.getElementById('cards-container');
-      cardsContainer.innerHTML = '';
+    const cardsContainer = document.getElementById('cards-container');
+    cardsContainer.innerHTML = '';
+    allButton.classList.add('btn-primary');
+    openButton.classList.remove('btn-primary');
+    closedButton.classList.remove('btn-primary');
     // console.log(cards);
 
     cards.forEach(card => {
 
-      
+
         const divCard = document.createElement('div');
 
         let priority = '';
@@ -103,9 +144,34 @@ const displayCardsinAll = (cards) => {
         }
 
         let greenBorder = '';
-        if(card.status === 'open'){
+        if (card.status === 'open') {
             greenBorder = 'border-t-4 border-green-500'
         }
+
+        const labels = card.labels.map(label => {
+            let color = '';
+            let icon = '';
+            if (label.toLowerCase() === 'bug') {
+                color = 'text-red-600 border-red-400 bg-red-100';
+                icon = '<i class="fa-solid fa-bug"></i>'
+            }
+            else if (label.toLowerCase() === 'enhancement') {
+                color = 'text-green-700 border-green-400 bg-green-100';
+                icon = '<i class="fa-solid fa-wand-magic-sparkles"></i>';
+            }
+            else if (label.toLowerCase() === 'help wanted') {
+                color = 'text-yellow-700 border-yellow-400 bg-yellow-100';
+                icon = '<i class="fa-regular fa-life-ring"></i>';
+            }
+            else if (label.toLowerCase() === 'documentation') {
+                color = 'text-blue-700 border-blue-400 bg-blue-100'
+            } else {
+                color = 'text-gray-700 border-gray-500 bg-gray-200'
+            }
+
+            return `<button class ="rounded-xl px-3 py-1 border ${color}"> ${icon} ${label}</button>`
+        }).join(' ');
+
         divCard.innerHTML = `
         
               <div class=" ${greenBorder} shadow-sm py-4 px-3 rounded-md h-full flex flex-col">
@@ -116,14 +182,15 @@ const displayCardsinAll = (cards) => {
                 <div>
                     <h2 class="text-[#1F2937] text-xl font-medium mb-2">${card.title}</h2>
                     <p class="text-[#64748B] mb-2"><small>${card.description}</small></p>
-                    <button class=" rounded-xl text-red-600 border-red-400 px-3 py-1 border bg-red-100 "><i class="fa-solid fa-bug"></i> BUG</button>
-                    <button class=" rounded-xl text-yellow-700 border border-yellow-400 py-1 px-3 bg-yellow-100"><i class="fa-regular fa-life-ring"></i> HELP WANTED</button>
+                    <div class="flex gap-5 mb-2">
+                        ${labels}
+                    </div>
                 </div>
                 <hr class="bg-gray-400 mt-5">
 
                 <div class="mt-3 flex justify-between">
                     <div>
-                    <p class="text-[#64748B]"><small> #1 by John_doe </small></p>
+                    <p class="text-[#64748B]"><small> ${card.id} by ${card.author} </small></p>
                     <p class="text-[#64748B]"><small> 1/15/2025 </small></p> 
                     </div>
 
